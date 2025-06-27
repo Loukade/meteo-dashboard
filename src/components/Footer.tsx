2749d38e7efb3251e4d028756cad350f2800c797
LucasReckall <lucas.jenvrain@gmail.com>
2025-06-27 13:17:10 +0200

    Add Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="w-full mt-8 border-t border-blue-300 dark:border-blue-800 pt-6 pb-4 flex flex-col items-center bg-transparent">
+      <span className="text-base md:text-lg font-medium text-blue-900 dark:text-blue-100 tracking-wide">
+        <span className="text-blue-500 font-mono text-lg align-middle">&lt;/&gt;</span> Réalisé  par <span className="font-bold text-blue-700 dark:text-blue-200">Lucas Jenvrain</span>
+      </span>
+    </footer>
+  )
+} 
